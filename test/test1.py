181b73a4381888cbaa6a8bf3c7252160efff0655
liushuochen <2524453728@qq.com>
2020-02-24 11:26:47 +0800

    定义存储服务器配置

--- test/test1.py
+++ test/test1.py
@@ -1,6 +1,40 @@
-import db.station
+from flask import Flask, request
+# from werkzeug import secure_filename
+# import werkzeug.datastructures.FileStorage
+
+# UPLOAD_FOLDER = '/Users/liushuochen/Desktop'
+# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#
+#
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        print("file:", file)
+        print("file.filename:", file.filename)
+        print(type(file))
+
+        file.save("/Users/liushuochen/Desktop/demo2.jpeg")
+        return "finish", 200
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 
 if __name__ == '__main__':
-    data = db.station.get_list()
-    print(data)
+    app.run()
